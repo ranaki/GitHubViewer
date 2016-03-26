e796b147521bc0fb222b69c843b3b889e6a4ef2c
Rick Baker <baker.rickw@gmail.com>
2016-03-26 11:15:08 -0600

    copy past updates showing use of log service

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 // Code goes here
-//Created with PluralSight turorial
 (function(){
   
 var app = angular.module("githubViewer",[]);
 
-var MainController = function($scope, $http) {
+var MainController = function($scope, $http, $interval, $log) {
 
   var onUserComplete = function(response) {
     $scope.user = response.data;
@@ -18,20 +17,42 @@ var MainController = function($scope, $http) {
   
   var onError = function(reason){
     $scope.error = "Could not fetch the data.";
+  };
+  
+  var decrementCountdown = function(){
+    $scope.countdown -= 1;
+    if($scope.countdown < 1){
+        $scope.search($scope.username);
+    }
+    
+  };
+  
+  var countdownInterval = null;
+  var startCountdown = function(){
+    countdownInterval = $interval(decrementCountdown, 1000, $scope.countdown);
   }
 
   $scope.search = function(username){
+    $log.info("Searching for " + username);
      $http.get("https://api.github.com/users/" + username)
     .then(onUserComplete,onError);
+    if (countdownInterval) {
+      $interval.cancel(countdownInterval);
+      $scope.countdown = null;
+    }
   };
   
 
   $scope.username = "angular";
   $scope.message = "GitHub Viewer";
   $scope.repoSortOrder ="-stargazers_count";
-
+  $scope.countdown = 5;
+  startCountdown();
+  
+  
 };
-
-app.controller("MainController",["$scope","$http", MainController]);
+app.controller("MainController", MainController);
+//app.controller("MainController",["$scope","$http", MainController]);
 
 }());
+
